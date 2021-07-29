c94f1bb00b5c92f48477680eaba1c175db023ed1
Juan José García Cantero <juanjose.garcia@epfl.ch>
2021-07-29 12:12:52 +0200

    Adds auto tetrahedralization of examples data.

--- examples/tetrahedralizeMesh.cpp
+++ examples/tetrahedralizeMesh.cpp
@@ -8,75 +8,95 @@
 
 #include <fstream>
 
-int main(int argc, char* argv[])
+void tetrahedralizeMesh(const std::string& file)
 {
-    std::string outFile("out_file.tet");
-    std::string inFile;
-    for (int i = 1; i < argc; ++i)
+    Eigen::MatrixXd sourceVertices;
+    Eigen::MatrixXi sourceFacets;
+
+    Eigen::MatrixXd destVertices;
+    Eigen::MatrixXi destTets;
+    Eigen::MatrixXi destFacets;
+
+    std::string outFile;
+    size_t extPos;
+    if ((extPos = file.find(".off")) != std::string::npos)
     {
-        std::string option(argv[i]);
-        if (option.compare("-out") == 0)
-        {
-            outFile = std::string(argv[i + 1]);
-            ++i;
-        }
-        else
-        {
-            inFile = std::string(argv[i]);
-        }
+        outFile = file.substr(0, extPos);
+        igl::readOFF(file.c_str(), sourceVertices, sourceFacets);
+    }
+    else if ((extPos = file.find(".ply")) != std::string::npos)
+    {
+        outFile = file.substr(0, extPos);
+        igl::readPLY(file.c_str(), sourceVertices, sourceFacets);
+    }
+    else if ((extPos = file.find(".obj")) != std::string::npos)
+    {
+        outFile = file.substr(0, extPos);
+        igl::readOBJ(file.c_str(), sourceVertices, sourceFacets);
+    }
+    else
+    {
+        return;
+    }
+    outFile.append(".tet");
+
+    igl::copyleft::tetgen::tetrahedralize(sourceVertices, sourceFacets,
+                                          "Qa0.01", destVertices, destTets,
+                                          destFacets);
+
+    std::ofstream os(outFile.c_str());
+    if (!os.is_open())
+    {
+        return;
     }
 
-    if (inFile.empty())
+    os << "tet " << destVertices.rows() << " " << destTets.rows() << "\n";
+    os << destVertices.format(Eigen::IOFormat(Eigen::FullPrecision,
+                                              Eigen::DontAlignCols, " ", "\n",
+                                              "", "", "", "\n"));
+    for (size_t i = 0; i < destTets.rows(); i++)
+    {
+        os << destTets(i, 0) << " " << destTets(i, 1) << " " << destTets(i, 2)
+           << " " << destTets(i, 3) << "\n";
+    }
+
+    std::cout << "Tetrahedralization sucess: " << destVertices.rows()
+              << " nodes, " << destTets.rows() << " tetrahedra." << std::endl;
+    std::cout << "Mesh saved: " << outFile << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
     {
         std::cerr << "Error Usage: " << argv[0]
                   << " mesh[.off|.ply|.obj] [-out out_file]" << std::endl;
         return 0;
     }
 
-    Eigen::MatrixXd sVertices;
-    Eigen::MatrixXi sFacets;
-
-    Eigen::MatrixXd vertices;
-    Eigen::MatrixXi tets;
-    Eigen::MatrixXi facets;
-
-    if (inFile.find(".off") != std::string::npos)
+    for (int i = 1; i < argc; ++i)
     {
-        igl::readOFF(inFile.c_str(), sVertices, sFacets);
-    }
-    else if (inFile.find(".ply") != std::string::npos)
-    {
-        igl::readPLY(inFile.c_str(), sVertices, sFacets);
-    }
-    else if (inFile.find(".obj") != std::string::npos)
-    {
-        igl::readOBJ(inFile.c_str(), sVertices, sFacets);
-    }
-    else
-    {
-        return 0;
+        tetrahedralizeMesh(std::string(argv[i]));
     }
 
-    igl::copyleft::tetgen::tetrahedralize(sVertices, sFacets, "a0.01", vertices,
-                                          tets, facets);
+    return 0;
 
-    std::ofstream os(outFile.c_str());
-    if (!os.is_open())
-    {
-        return 0;
-    }
+    // if (inFile.find(".off") != std::string::npos)
+    // {
+    //     igl::readOFF(inFile.c_str(), sVertices, sFacets);
+    // }
+    // else if (inFile.find(".ply") != std::string::npos)
+    // {
+    //     igl::readPLY(inFile.c_str(), sVertices, sFacets);
+    // }
+    // else if (inFile.find(".obj") != std::string::npos)
+    // {
+    //     igl::readOBJ(inFile.c_str(), sVertices, sFacets);
+    // }
+    // else
+    // {
+    //     return 0;
+    // }
 
-    os << "tet " << vertices.rows() << " " << tets.rows() << "\n";
-    os << vertices.format(Eigen::IOFormat(Eigen::FullPrecision,
-                                          Eigen::DontAlignCols, " ", "\n", "",
-                                          "", "", "\n"));
-    for (size_t i = 0; i < tets.rows(); i++)
-    {
-        os << tets(i, 0) << " " << tets(i, 1) << " " << tets(i, 2) << " "
-           << tets(i, 3) << "\n";
-    }
-
-    std::cout << "Mesh tetrahedalized with: " << vertices.rows() << " nodes, "
-              << tets.rows() << " tetrahedra." << std::endl;
-    return 1;
+    // return 1;
 }
