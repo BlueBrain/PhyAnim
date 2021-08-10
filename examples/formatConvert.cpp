8fed05e6065b78a3a95590aeca9c3bb1d597009c
Juan Jose Garcia Cantero <juanjose.garcia@epfl.ch>
2021-08-10 10:11:43 +0200

    Adds format convert app. Adds neuron tetrahedral example meshes.

---
+++ examples/formatConvert.cpp
@@ -0,0 +1,88 @@
+#include <Mesh.h>
+
+#include <chrono>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage error:\nUse: " << argv[0]
+                  << " [-tet] [-off] file_name" << std::endl;
+        return 0;
+    }
+
+    std::string ext(".off");
+    std::vector<std::string> files;
+
+    for (uint32_t i = 1; i < argc; ++i)
+    {
+        std::string option(argv[i]);
+        if (option.compare("-tet") == 0)
+        {
+            ext = std::string(".tet");
+        }
+        else if (option.compare("-off") == 0)
+        {
+            ext = std::string(".off");
+        }
+        else
+        {
+            files.push_back(option);
+        }
+    }
+
+    for (uint32_t i = 0; i < files.size(); ++i)
+    {
+        size_t extPos = 0;
+        phyanim::MeshPtr mesh = nullptr;
+        std::string baseFile;
+        if ((extPos = files[i].find(".node")) != std::string::npos)
+        {
+            if ((i + 1) < files.size() &&
+                files[i + 1].find(".ele") != std::string::npos)
+            {
+                mesh = new phyanim::Mesh();
+                mesh->load(files[i], files[i + 1]);
+                baseFile = files[i].substr(0, extPos);
+                ++i;
+            }
+        }
+        else if ((extPos = files[i].find(".off")) != std::string::npos)
+        {
+            mesh = new phyanim::Mesh();
+            mesh->load(files[i]);
+            baseFile = files[i].substr(0, extPos);
+        }
+        else if ((extPos = files[i].find(".ply")) != std::string::npos)
+        {
+            mesh = new phyanim::Mesh();
+            mesh->load(files[i]);
+            baseFile = files[i].substr(0, extPos);
+        }
+        else if ((extPos = files[i].find(".obj")) != std::string::npos)
+        {
+            mesh = new phyanim::Mesh();
+            mesh->load(files[i]);
+            baseFile = files[i].substr(0, extPos);
+        }
+        else if ((extPos = files[i].find(".tet")) != std::string::npos)
+        {
+            mesh = new phyanim::Mesh();
+            mesh->load(files[i]);
+            baseFile = files[i].substr(0, extPos);
+        }
+        else
+        {
+            std::cout << "unknown format file: " << files[i] << std::endl;
+        }
+
+        if (mesh)
+        {
+            baseFile.append(ext);
+            mesh->write(baseFile);
+            std::cout << "Saved file: " << baseFile << std::endl;
+            delete mesh;
+        }
+    }
+}
